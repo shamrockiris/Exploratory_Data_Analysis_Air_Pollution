dbec40d347aa6cadb8c6a2b3d9d12f77c17a65ec
shamrockiris <wangxiaowen.iris@gmail.com>
2015-04-27 12:58:50 -0700

    simplify

--- plot1.R
+++ plot1.R
@@ -2,12 +2,9 @@
 unzip("exdata-data-NEI_data.zip")
 NEI <- readRDS("summarySCC_PM25.rds")
 PMbyYear <- tapply(NEI$Emissions,NEI$year,sum)
-year <- names(PMbyYear)
-emission <- as.data.frame(cbind(PMbyYear,year),stringsAsFactors=FALSE)
-emission$year <- as.Date(emission$year,"%Y")
-emission$PMbyYear <- as.numeric(emission$PMbyYear)
+emission <- data.frame(year=names(PMbyYear),PMbyYear)
 png(filename="plot1.png",width=480,height=480,units="px")
-plot(x=emission$year,y=emission$PMbyYear,col="blue",pch=16,xlab="Year",ylab="PM2.5(tons)",
+barplot(height=emission$PMbyYear,col="blue",xlab="Year",ylab="PM2.5(tons)",
      main="Total Pollute Emissions by Year ")
 model <- line(emission$PMbyYear~emission$year)
 abline(coef(model),lwd=2)
--- plot2.R
+++ plot2.R
@@ -2,12 +2,9 @@ unzip("exdata-data-NEI_data.zip")
 NEI <- readRDS("summarySCC_PM25.rds")
 Baltimore <- NEI[NEI$fips=="24510",]
 PMbyYear <- tapply(Baltimore$Emissions,Baltimore$year,sum)
-year <- names(PMbyYear)
-emission <- as.data.frame(cbind(PMbyYear,year),stringsAsFactors=FALSE)
-emission$year <- as.Date(emission$year,"%Y")
-emission$PMbyYear <- as.numeric(emission$PMbyYear)
+emission <- data.frame(year=names(PMbyYear),PMbyYear)
 png(filename="plot2.png",width=480,height=480,units="px")
-plot(x=emission$year,y=emission$PMbyYear,col="blue",pch=16,xlab="Year",ylab="PM2.5(tons)",
+barplot(emission$PMbyYear,col="blue",xlab="Year",ylab="PM2.5(tons)",
      main="Total Pollute Emissions by Year for Baltimore, MaryLand ")
 model <- line(emission$PMbyYear~emission$year)
 abline(coef(model),lwd=2)
