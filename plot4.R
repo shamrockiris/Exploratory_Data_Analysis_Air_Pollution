de28b692d88034d366e7c16b735e9ade10b83a9f
shamrockiris <wangxiaowen.iris@gmail.com>
2015-04-27 12:57:01 -0700

    simplify

--- plot4.R
+++ plot4.R
@@ -11,14 +11,13 @@ dataCoal <- subset(NEI,(SCC %in% pCoal))
 
 # calculate the emission by these source per year
 PMbyYear <- tapply(dataCoal$Emissions,dataCoal$year,sum)
-year <- names(PMbyYear)
-emission <- as.data.frame(cbind(PMbyYear,year),stringsAsFactors=FALSE)
-emission$year <- as.Date(emission$year,"%Y")
-emission$PMbyYear <- as.numeric(emission$PMbyYear)
+
+emission <- data.frame(year=names(PMbyYear),PMbyYear)
+
 
 # plot
 png(filename="plot4.png",width=480,height=480,units="px")
-plot(x=emission$year,y=emission$PMbyYear,col="blue",pch=16,xlab="Year",ylab="PM2.5(tons)",
+barplot(emission$PMbyYear,col="blue",xlab="Year",ylab="PM2.5(tons)",
      main="Coal Pollute Emissions by Year ")
 model <- line(emission$PMbyYear~emission$year)
 abline(coef(model),lwd=2)
--- plot5.R
+++ plot5.R
@@ -11,14 +11,13 @@ dataMotor <- subset(NEI,(SCC %in% pMotor) & fips == "24510")
 
 # calculate the emission by these source per year
 PMbyYear <- tapply(dataMotor$Emissions,dataMotor$year,sum)
-year <- names(PMbyYear)
-emission <- as.data.frame(cbind(PMbyYear,year),stringsAsFactors=FALSE)
-emission$year <- as.Date(emission$year,"%Y")
-emission$PMbyYear <- as.numeric(emission$PMbyYear)
+
+emission <- data.frame(year=names(PMbyYear),PMbyYear)
+
 
 # plot
 png(filename="plot5.png",width=480,height=480,units="px")
-plot(x=emission$year,y=emission$PMbyYear,col="blue",pch=16,xlab="Year",ylab="PM2.5(tons)",
+barplot(emission$PMbyYear,col="blue",xlab="Year",ylab="PM2.5(tons)",
      main="Motor Vehicle Pollution by Year ")
 model <- line(emission$PMbyYear~emission$year)
 abline(coef(model),lwd=2)
